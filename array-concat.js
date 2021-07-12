0237c609dcd61ccb743b7601ed68b2e256596891
Rafay Siddiqui <rafays.siddiqui@gmail.com>
2021-07-12 10:03:00 +0000

    concatenating two arrays

---
+++ array-concat.js
@@ -0,0 +1,12 @@
+function concat(arr1, arr2) {
+  let newArr = arr1
+  arr2.forEach(function (num) {
+    newArr.push(num);
+  })
+  return newArr;
+}
+
+console.log(concat([1, 2, 3], [4, 5, 6]), "=?", [1, 2, 3, 4, 5, 6]);
+console.log(concat([0, 3, 1], [9, 7, 2]), "=?", [0, 3, 1, 9, 7, 2]);
+console.log(concat([], [9, 7, 2]), "=?", [9, 7, 2]);
+console.log(concat([5, 10], []), "=?", [5, 10]);
