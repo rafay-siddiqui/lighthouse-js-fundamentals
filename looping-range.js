5f25abff777c9e876206cbd2e43d2858b8f542f8
Rafay Siddiqui <rafays.siddiqui@gmail.com>
2021-07-12 09:40:25 +0000

    loop by start,end,step

---
+++ looping-range.js
@@ -0,0 +1,21 @@
+function range(start, end, step) {
+  result = [];
+  if (start == undefined || end == undefined || step == undefined) {
+    return [];
+  } else if (start > end) {
+    return [];
+  } else if (step <= 0) {
+    return [];
+  } else {
+    num = start;
+    while (num <= end) {
+      result.push(num);
+      num += step;
+    }
+  }
+  return result;
+}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
